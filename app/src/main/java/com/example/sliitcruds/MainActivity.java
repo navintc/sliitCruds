71b33e8c3279a892b83505dfbd2a31d902cdc85e
navintc <navin.thamindu@gmail.com>
2021-10-30 00:16:14 +0530

    adding fragments and navgraph

--- app/src/main/java/com/example/sliitcruds/MainActivity.java
+++ app/src/main/java/com/example/sliitcruds/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sliitcruds;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void changeFragment(View view){
+        Fragment fragment;
+
+        if (view == findViewById(R.id.button)){
+            fragment = new fragment1();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault, fragment);
+            ft.commit();
+        }
+
+        if (view == findViewById(R.id.button2)){
+            fragment = new fragment2();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault, fragment);
+            ft.commit();
+        }
+    }
+
 }
